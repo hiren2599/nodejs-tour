97c98922c004594ba9f9544ed4380766ae4cd963
hiren <hirenlineshbhaipatel@gmail.com>
2020-03-31 12:43:25 +0530

    stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -58,7 +58,7 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))
     .id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
